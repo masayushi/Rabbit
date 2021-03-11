86c90a39118c7cc58eb56b1dc26d9efc0d075106
masayushi <65478781+masayushi@users.noreply.github.com>
2021-03-11 18:28:53 +0800

    調整圖片大小

---
+++ Rabbit/Assets/Script/rabbit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class rabbit : MonoBehaviour
+{
+    private void jump()
+    {
+        Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void idou()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, speed, 0);
+    }
+
+
+    private void Update()
+    {
+        jump();
+        idou();
+    }
+
+
+}
