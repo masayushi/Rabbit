2eb1f53fc614f760eeebe2c5bb8b1cebef2b3a3e
masayushi <65478781+masayushi@users.noreply.github.com>
2021-03-11 21:44:09 +0800

    場景完成、程式

--- Rabbit/Assets/Script/rabbit.cs
+++ Rabbit/Assets/Script/rabbit.cs
@@ -2,23 +2,44 @@
 
 public class rabbit : MonoBehaviour
 {
-    private void jump()
+    #region 欄位
+    [Header("移動距離"), Range(0, 5)]
+    public float idou = 0.5f;
+
+    [Header("垂直跳躍高度"), Range(0, 10)]
+    public float jump = 1;
+
+    [Header("跳躍距離"), Range(0, 15)]
+    public float jumpkyouri = 1.2f;
+
+    public Transform Rabbit;
+
+    #endregion
+
+    private void Start()
     {
-        Input.GetKeyDown(KeyCode.Space);
+        print("兔子座標" + Rabbit.position);
+        Move();
     }
 
-    private void idou()
+
+    private void Move()
     {
-        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        transform.Rotate(0, speed, 0);
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.right);
+        Gizmos.DrawRay(transform.position, Vector3.left);
+        Gizmos.DrawRay(transform.position, Vector3.up);
+        Gizmos.DrawRay(transform.position, Vector3.down);
     }
 
 
-    private void Update()
-    {
-        jump();
-        idou();
-    }
+
+
 
 
 }
