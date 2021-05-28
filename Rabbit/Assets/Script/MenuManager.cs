de610cdb3a3cea74c4d1bd4c5984f3e0cb317ada
masayushi <4a1e0010@stust.edu.tw>
2021-05-28 23:27:24 +0800

    添加轉場

--- Rabbit/Assets/Script/MenuManager.cs
+++ Rabbit/Assets/Script/MenuManager.cs
@@ -3,7 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // public 公開，允許按鈕呼叫
+    [SerializeField] RectTransform fader;
+
+    #region 開啟與結束遊戲
 
     /// <summary>
     /// 延遲呼叫，等音效播完
@@ -36,7 +38,7 @@ public class MenuManager : MonoBehaviour
         //SceneManager.LoadScene("遊戲場景");
 
         SceneManager.LoadScene(1);
-        
+
     }
 
     /// <summary>
@@ -47,5 +49,50 @@ public class MenuManager : MonoBehaviour
         //退出遊戲
         //語法：應用程式.離開遊戲(); -> 應用程式 的 離開遊戲 
         Application.Quit();
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setOnComplete(() =>
+       {
+           fader.gameObject.SetActive(false);
+       });
+    }
+
+
+    public void OpenMenuScene()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, Vector3.zero, 0f);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setOnComplete(() =>
+        {
+            SceneManager.LoadScene(0);
+        });
+    }
+
+
+    private void OpenGameScene()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, Vector3.zero, 0f);
+
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setOnComplete(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
--- Rabbit/Assets/Script/SceneChanger.cs
+++ Rabbit/Assets/Script/SceneChanger.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
@@ -6,19 +8,40 @@ public class SceneChanger : MonoBehaviour
     [Header("連接到其他場景")]
     public string GoToScene;
 
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("玩家"))
         {
-            SceneManager.LoadScene(GoToScene);
+            // SceneManager.LoadScene(GoToScene);
+            StartCoroutine(FadeCo());
         }
     }
 
-
-    private void Quitgame()
+    public IEnumerator FadeCo()
     {
-        //退出遊戲
-        //語法：應用程式.離開遊戲(); -> 應用程式 的 離開遊戲 
-        Application.Quit();
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(GoToScene);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
     }
 }
